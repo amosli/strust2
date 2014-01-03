38332f7998dc906060afaa744d6295d28391d72c
liyayun <amosli@infomorrow.com>
2014-01-03 23:46:45 +0800

    strust2学习

---
+++ src/main/java/com/amos/web/action/HelloWorldAction.java
@@ -0,0 +1,13 @@
+package com.amos.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+//struts2的控制器 
+public class HelloWorldAction extends ActionSupport{
+	private static final long serialVersionUID = 6872366878758961847L;
+	//重写execute()方法
+	public String execute() throws Exception {
+		System.out.println("欢迎使用struts2!");
+		return super.execute();
+	}
+}
