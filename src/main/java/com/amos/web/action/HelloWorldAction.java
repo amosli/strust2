e2b2cba226a77f5c3b0dd57e5f34c90e34787f73
liyayun <amosli@infomorrow.com>
2014-01-05 02:24:39 +0800

    tag

--- src/main/java/com/amos/web/action/HelloWorldAction.java
+++ src/main/java/com/amos/web/action/HelloWorldAction.java
@@ -6,8 +6,8 @@ import com.opensymphony.xwork2.ActionSupport;
 public class HelloWorldAction extends ActionSupport{
 	private static final long serialVersionUID = 6872366878758961847L;
 	//重写execute()方法
-	public String execute() throws Exception {
+	public String execute2() throws Exception {
 		System.out.println("欢迎使用struts2!");
-		return "success";
+		return "success2";
 	}
 }
