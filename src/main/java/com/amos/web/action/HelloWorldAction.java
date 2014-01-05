712bf9fc990a97060992da5868d671e70f219350
liyayun <amosli@infomorrow.com>
2014-01-05 16:07:22 +0800

    struts2配置详解

--- src/main/java/com/amos/web/action/HelloWorldAction.java
+++ src/main/java/com/amos/web/action/HelloWorldAction.java
@@ -5,9 +5,13 @@ import com.opensymphony.xwork2.ActionSupport;
 //struts2的控制器 
 public class HelloWorldAction extends ActionSupport{
 	private static final long serialVersionUID = 6872366878758961847L;
+	
+	public HelloWorldAction(){
+		System.out.println("hello actions:"+this.hashCode());
+	}
 	//重写execute()方法
-	public String execute2() throws Exception {
+	public String execute() throws Exception {
 		System.out.println("欢迎使用struts2!");
-		return "success2";
+		return "success";
 	}
 }
